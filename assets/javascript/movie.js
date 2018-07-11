e2c0dcf90ae6bcc5c4537a8a78773c1ce05713a9
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-07-11 18:47:48 -0400

    Suggest 5 movies

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -1,9 +1,29 @@
 $("#submitMovie").on("click", function () {
 
     //getting the movie name from the main search page
-    var movieName = $("#textarea1").val().trim();
+    
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api.themoviedb.org/3/movie/now_playing?page=1&language=en-US&api_key=73e53def52ff9a718eda18d64097f3bc",
+      "method": "GET",
+      "headers": {},
+      "data": "{}"
+    }
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    }).then(function(response) {
+      console.log(response);
+      for (i=0;i<5;i++) {
+ 
+      
+    
+    
+    var movieName = response.results[i].title;
+    console.log(movieName);
     var final_id = "";
-    function havefinal() {
+    // function havefinal() {
      //first ajax call to get the movie id
      var queryURL = " https://api.themoviedb.org/3/search/movie?api_key=73e53def52ff9a718eda18d64097f3bc&query=" + movieName;
      $.ajax({
@@ -19,22 +39,24 @@ $("#submitMovie").on("click", function () {
          url: queryURL,
          method: "GET"
        }).then(function(response) {
-         var num = JSON.stringify(response.results.length);
-         for (var i = 0; i <= num; i++) {
-           var key_first = JSON.stringify(response.results[i].key);
+        //  var num = JSON.stringify(response.results.length);
+        //  for (var i = 0; i <= num; i++) {
+           var key_first = JSON.stringify(response.results[1].key);
            var key_final = eval(key_first);
            var newdiv = $("<div><br><br>");
            //using that key we are showing youtube video
            newdiv.append(
              "<iframe width='280' height='220' src='https://www.youtube.com/embed/" + key_final +"' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>"
            );
-           newdiv.append("<br><br><br><br>");
+           newdiv.append("<br>");
            $(".poster").prepend(newdiv);
-         }
+        //  }
        });
      });
     }
-    havefinal();
+  // }
+  });
+    // havefinal();
     
     
             })
